a0adc79a000638c8a7b77fcdba23667340112ccf
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-07 18:01:09 +0200

    termine esercizio

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const chilometri = prompt ("Quanti chilometri devi percorrere?")
 
-console.log(chilometri)
-
 const eta = prompt ("Quanti anni ha il passeggero?")
-
-console.log(eta)
 
 const numero = 0.21
 
 let risultato = numero * chilometri
-
-console.log(risultato)
 
 let sconto20 = risultato * 20 / 100 
 
