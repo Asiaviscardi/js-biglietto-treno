bc3730fe9ed87e4d7ae5bc449d3c5f6c9ad7ce3c
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-07 16:32:16 +0200

    inserimento calcolo dei centesimi per chilometro

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ console.log(chilometri)
 const eta = prompt ("Quanti anni ha il passeggero?")
 
 console.log(eta)
+
+const numero = 0.21
+
+let risultato = numero * chilometri
+
+console.log(risultato)
