94f05b0a3a1d4a90cebd96a54585c7ce5088b8ea
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-07 18:00:29 +0200

    inserimento tofixed

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ let sconto20 = risultato * 20 / 100
 let sconto40 = risultato * 40 / 100
 
 if( eta < 18 ){
-    prezzo.innerHTML = "il costo del biglietto è" + (risultato - sconto20)
+    prezzo.innerHTML = "il costo del biglietto è" + (risultato - sconto20).toFixed(2)
 }else if( eta >= 65){
-    prezzo.innerHTML = "il costo del biglietto è" + (risultato - sconto40)
+    prezzo.innerHTML = "il costo del biglietto è" + (risultato - sconto40).toFixed(2)
 }else{
-   prezzo.innerHTML = "il costo del biglietto è" + (risultato)
+   prezzo.innerHTML = "il costo del biglietto è" + (risultato).toFixed(2)
 }
 
 document.getElementById ("prezzo")
